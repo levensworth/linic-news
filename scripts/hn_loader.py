037e49a373ec47dfcf2927643560bb1acd271545
Santiago Bassani <santiago.bassani96@gmail.com>
2025-04-27 01:06:01 +0100

    [ADD] new image model

--- scripts/hn_loader.py
+++ scripts/hn_loader.py
@@ -369,8 +369,8 @@ def generate_lead(article):
 def generate_image(article_summary):
     client = openai.OpenAI()
     result = client.images.generate(
-    model="dall-e-2",
-    size='512x512',
+    model="gpt-image-1",
+    size='1024x1024',
     response_format='b64_json',
     prompt="Generate a cover image in  8bit style for an article which contains the following summary: " + article_summary
 )
